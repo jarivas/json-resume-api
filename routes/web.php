ffc8fa5199a082e4447dcb1b292de39cf887af37
Jose Rivas <me@jarivas.work>
2026-02-17 21:43:30 +0000

    Added Routes in web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Basic\Read;
+use App\Http\Controllers\Basic\ReadOne;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn ()=> redirect('/up'))->name('health');
+Route::get('/', fn ()=> redirect('/up'))
+    ->name('health');
+
+Route::get('/basics', Read::class);
+
+Route::get('/basics/{id}', ReadOne::class)
+    ->whereUlid('id');
